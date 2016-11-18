3bd656ec8e7458b024e3550a8dff74c14b04f3ec
Li Di <i@szg.li>
2016-11-18 13:31:22 +0800

    add upload file c code.

--- cmd/newserver.go
+++ cmd/newserver.go
@@ -1,6 +1,17 @@
 package main
 
 
+import (
+    "fmt"
+    "os"
+    "log"
+    "io"
+    "net/http"
+    "html/template"
+    "github.com/gin-gonic/gin"
+    "unsafe"
+)
+
 /*
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,20 +29,55 @@ package main
 #include "client_func.h"
 #include "client_global.h"
 
+
+char file_id[128];
+
+int fdfs_upload_file(char *conf_filename, char *local_filename)
+{
+	char group_name[FDFS_GROUP_NAME_MAX_LEN + 1];
+	ConnectionInfo *pTrackerServer;
+	int result;
+	int store_path_index;
+	ConnectionInfo storageServer;
+	//char file_id[128];
+
+
+	conf_filename = argv[1];
+	if ((result=fdfs_client_init(conf_filename)) != 0)
+	{
+		return result;
+	}
+
+	pTrackerServer = tracker_get_connection();
+	if (pTrackerServer == NULL)
+	{
+		fdfs_client_destroy();
+		return errno != 0 ? errno : ECONNREFUSED;
+	}
+
+	*group_name = '\0';
+	if ((result=tracker_query_storage_store(pTrackerServer, \
+	                &storageServer, group_name, &store_path_index)) != 0)
+	{
+		fdfs_client_destroy();
+		return result;
+	}
+
+	result = storage_upload_by_filename1(pTrackerServer, \
+			&storageServer, store_path_index, \
+			local_filename, NULL, \
+			NULL, 0, group_name, file_id);
+
+	tracker_disconnect_server_ex(pTrackerServer, true);
+	fdfs_client_destroy();
+
+	return result;
+}
+
 #cgo CFLAGS: -I/usr/include/fastcommon -I/usr/include/fastdfs
 #cgo LDFLAGS: -L/usr/lib64 -lpthread -lfastcommon -lfdfsclient
 */
 import "C"
-
-import (
-    "fmt"
-    "os"
-    "log"
-    "io"
-    "net/http"
-    "html/template"
-    "github.com/gin-gonic/gin"
-)
 
 func main() {
     router := gin.Default()
@@ -56,10 +102,20 @@ func main() {
             log.Fatal(err)
         }
 
-        result := C.fdfs_client_init("/etc/fdfs/client.conf")
-        fmt.Println("fdfs client init result is", int(result))
+        confstr := C.CString("/etc/fdfs/client.conf")
+        defer C.free(unsafe.Pointer(confstr))
+        localstr := C.CString("./test/" + filename)
+        defer C.free(unsafe.Pointer(localstr))
+        result := int(C.fdfs_upload_file(confstr, localstr))
 
-        c.JSON(http.StatusOK, gin.H{"fileid": "i am a fileid", "key": "i am a key",})
+        if result == 0 {
+            fmt.Println("file id is", C.GoString(C.file_id))
+            c.JSON(http.StatusOK, gin.H{"result": "success", "file_id": "i am a file id", "key": "i am a key",})
+        } else {
+            c.JSON(http.StatusOK, gin.H{"result": "fail",})
+        }
+
+
     })
 
     router.Run(":8080")
