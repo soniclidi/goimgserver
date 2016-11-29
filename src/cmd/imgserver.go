8620e60b650fd2d86d668e875a6ad19d3667ca82
Li Di <i@szg.li>
2016-11-29 16:31:32 +0800

    change the method of gen token.

--- src/cmd/imgserver.go
+++ src/cmd/imgserver.go
@@ -12,13 +12,15 @@ import (
     "strconv"
     "crypto/md5"
     "encoding/hex"
-    "math/rand"
     "time"
     "flag"
     "mime"
+    "encoding/base64"
+    "strings"
 
     "config"
 
+    "github.com/pborman/uuid"
     "gopkg.in/mgo.v2"
     "github.com/gin-gonic/gin"
     "gopkg.in/mgo.v2/bson"
@@ -174,7 +176,6 @@ func main() {
     confStr := C.CString(conf.FdfsClient.ConfigFile)
     defer C.free(unsafe.Pointer(confStr))
     result := int(C.init_fdfs(confStr))
-
     if result != 0 {
         panic("init fdfs error!")
     }
@@ -229,9 +230,8 @@ func main() {
             }
         }
 
-
         if result == 0 {
-            fileToken := string(Krand(10, KC_RAND_KIND_ALL))
+            fileToken := genToken()
             fmt.Println("file token:", fileToken)
             newFile := &File{
                 File_id: fileId,
@@ -333,23 +333,7 @@ func main() {
     router.Run(":" + strconv.Itoa(conf.WebServer.Port))
 }
 
-const (
-    KC_RAND_KIND_NUM   = 0  // 纯数字
-    KC_RAND_KIND_LOWER = 1  // 小写字母
-    KC_RAND_KIND_UPPER = 2  // 大写字母
-    KC_RAND_KIND_ALL   = 3  // 数字、大小写字母
-)
-
-func Krand(size int, kind int) []byte {
-    ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
-    is_all := kind > 2 || kind < 0
-    rand.Seed(time.Now().UnixNano())
-    for i :=0; i < size; i++ {
-        if is_all { // random ikind
-            ikind = rand.Intn(3)
-        }
-        scope, base := kinds[ikind][0], kinds[ikind][1]
-        result[i] = uint8(base+rand.Intn(scope))
-    }
-    return result
+func genToken() string {
+    id := uuid.NewRandom()
+    return strings.TrimRight(base64.URLEncoding.EncodeToString([]byte(id)), "=")
 }
